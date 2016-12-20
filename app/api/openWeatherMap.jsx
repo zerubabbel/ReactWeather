88aa549a8b69ef818d4608abf60d649664be6330
zerubabbel <achiappetta90@gmail.com>
2016-12-19 21:08:53 -0800

    initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,23 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=8cb058981da21aef94e675c4b4f574b9&units=imperial';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+      debugger;
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+
+    }, function (res) {
+      throw new Error(res.data.message);
+
+    });
+  }
+}
