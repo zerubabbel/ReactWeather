991eb5f69c629611ad16aefec3168e98e59b9c09
zerubabbel <achiappetta90@gmail.com>
2016-12-22 07:36:25 -0800

    more styling

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -8,7 +8,7 @@ var WeatherForm = React.createClass({
 
     if (location.length > 0) {
       this.refs.location.value = '';
-      this.props.onSearch(location); 
+      this.props.onSearch(location);
     }
   },
 
@@ -19,7 +19,7 @@ var WeatherForm = React.createClass({
       <div>
         <form onSubmit={this.onFormSubmit}>
           <input type="text" ref="location"/>
-          <button>Get Weather</button>
+          <button className="button expanded hollow">Get Weather</button>
         </form>
       </div>
     );
